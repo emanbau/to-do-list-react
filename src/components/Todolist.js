3efcaa6ef8a1ae27063e678caf86d3cc8b9bd80f
Eman Bau <emanbautista410@gmail.com>
2020-09-23 13:53:32 -0400

    basic functionality

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import Todoform from './Todoform'
+import Todo from './Todo'
 
 function Todolist() {
     const [todos, setTodos] = useState([])
@@ -15,10 +16,36 @@ function Todolist() {
         setTodos(newTodos)
     }
 
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)) {
+            return
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr);
+    }
+
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete
+            }
+            return todo
+        })
+        setTodos(updatedTodos);
+    }
+
     return (
         <div>
             <h1>To Do List</h1>
             <Todoform onSubmit={addTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
         </div>
     )
 }
