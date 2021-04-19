12581387d1b28acd40736cf4856bfe745c0d6c07
Eman Bau <emanbautista410@gmail.com>
2021-04-19 09:09:52 -0400

    Build

---
+++ build/precache-manifest.722a97ce53b6d83ebb56b5dd2ef0b0c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f62d17e4760345cc933f68ee23b3be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e56eb6731ba58bc5bd93",
+    "url": "/static/css/main.a9168abd.chunk.css"
+  },
+  {
+    "revision": "9ddc97fb62c33618ddb9",
+    "url": "/static/js/2.a337deb8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a337deb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56eb6731ba58bc5bd93",
+    "url": "/static/js/main.a447d7dc.chunk.js"
+  },
+  {
+    "revision": "2b94e1a4fc4635c7be73",
+    "url": "/static/js/runtime-main.2714f617.js"
+  }
+]);
